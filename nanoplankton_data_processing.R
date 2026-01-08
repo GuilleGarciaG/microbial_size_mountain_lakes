05cb7cbba232708df18e9a993ea7137a527d9912
Guillermo Garcia <guillermogarcia@Guillermos-MacBook-Pro.local>
2026-01-08 13:47:41 +0100

    first commit1

--- nanoplankton_data_processing.R
+++ nanoplankton_data_processing.R
@@ -7,7 +7,7 @@
 # ideal to analyse nanoplankton communities
 #
 # Author: Guillermo García-Gómez (guillegar.gz@gmail.com)
-# Date: 10/12/2025
+# Date: 08/01/2026
 # Operating System: MackBook-Pro 14; macOS, Darwin Kernel Version 24.4.0
 # ------------------------------------------------------------------------------
 # Cite as:
